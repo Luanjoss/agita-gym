ae2dc5dfb24f398d9409bfde93768bcc855f4ba6
Luanjoss <luanmicro890@gmail.com>
2026-05-13 17:56:50 -0300

    feat: habilita CORS no main.ts para integração com o front-end

--- backend-agita/src/main.ts
+++ backend-agita/src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors(); // Habilita CORS para permitir requisições de outros domínios
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
