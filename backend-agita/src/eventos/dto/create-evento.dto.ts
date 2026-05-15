985fc69363aec1668aac68366d2937ec87497ff1
Luanjoss <luanmicro890@gmail.com>
2026-05-15 14:24:19 -0300

    docs: implementa swagger ui e documentacao base da api

--- backend-agita/src/eventos/dto/create-evento.dto.ts
+++ backend-agita/src/eventos/dto/create-evento.dto.ts
@@ -6,19 +6,32 @@ import {
   MinLength,
   IsOptional,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEventoDto {
+  @ApiProperty({
+    description: 'Título do evento',
+    example: 'Aula de Yoga',
+  })
   @IsString({ message: 'O nome deve ser uma string.' })
   @IsNotEmpty({ message: 'O nome do evento não pode estar vazio.' })
   @MinLength(5, {
     message: 'O nome do evento deve ter pelo menos 5 caracteres.',
-  }) // Ajustei para 5 conforme a mensagem
+  }) // Ajustado para 5
   titulo: string;
 
+  @ApiProperty({
+    description: 'Data do evento no formato ISO8601',
+    example: '2024-12-31T18:00:00Z',
+  })
   @IsISO8601({}, { message: 'A data deve estar no formato ISO8601.' })
   @IsNotEmpty({ message: 'A data é obrigatória.' })
   dataEvento: string;
 
+  @ApiProperty({
+    description: 'Descrição opcional do evento',
+    example: 'Uma aula de yoga para relaxar e fortalecer o corpo.',
+  })
   @IsString()
   @IsOptional() // Isso permite que o evento seja criado sem descrição se quiser
   descricao?: string;
--- backend-agita/src/main.ts
+++ backend-agita/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('AGITA Ginástica - API')
+    .setDescription('Documentação do sistema de gestão da AGITA')
+    .setVersion('1.0')
+    .addTag('eventos')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   await app.listen(process.env.PORT ?? 3000);
 }
